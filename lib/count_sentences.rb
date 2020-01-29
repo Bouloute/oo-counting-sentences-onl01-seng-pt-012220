2ab811a2df5f86139c29a6c8d392dd01d976d358
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-29 15:22:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?(sentence)
-    sentence.end_with(".")    
+    sentence.end_with(".")
   end
 
   def question?(sentence)
-    sentence.end_with("?")     
+    sentence.end_with("?")
   end
 
   def exclamation?(sentence)
-    sentence.end_with("!") 
+    sentence.end_with("!")
   end
 
   def count_sentences
